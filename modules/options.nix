ed3b9cf6119e9d45dd3e0fe8fdea2ff9b1614938
Elis Odenhage <teknikgillaren@gmail.com>
2023-02-01 15:13:47 +0100

    make the config.dotfiles.dir point to the right dir

--- modules/options.nix
+++ modules/options.nix
@@ -6,11 +6,11 @@ with lib.my; {
     user = mkOpt attrs { };
 
     dotfiles = {
-      dir = mkOpt path (removePrefix "/mnt"
-        (findFirst pathExists (toString ../.) [
-          "/mnt/etc/dotfiles"
-          "/etc/dotfiles"
-        ]));
+      dir = mkOpt path
+        (removePrefix "/home/${config.user.name}"
+          (findFirst pathExists (toString ../.) [
+            "/home/${config.user.name}/new-config"
+          ]));
       binDir = mkOpt path "${config.dotfiles.dir}/bin";
       configDir = mkOpt path "${config.dotfiles.dir}/config";
       modulesDir = mkOpt path "${config.dotfiles.dir}/modules";
