e4049d82ae1f2485d84fb1892a2d2b68c10da283
Elis Odenhage <teknikgillaren@gmail.com>
2023-02-01 15:17:10 +0100

    remove redshift

--- modules/desktop/herbstluftwm.nix
+++ modules/desktop/herbstluftwm.nix
@@ -11,7 +11,6 @@ in {
   config = mkIf cfg.enable {
     services = {
       picom.enable = true;
-      redshift.enable = true;
       xserver = {
         enable = true;
         displayManager = {
