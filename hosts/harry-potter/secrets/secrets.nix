63a3372941088b92ff40f277b5ed1a29173b4864
Elis Odenhage <teknikgillaren@gmail.com>
2023-06-17 17:45:43 +0200

    flake: update all inputs

--- hosts/harry-potter/secrets/secrets.nix
+++ hosts/harry-potter/secrets/secrets.nix
@@ -2,4 +2,5 @@ let
   key =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIID1bhNzIcUZNgOAISOb57K7tiXzxLPHxG+hGnjQA68h Encrypt Secrets Nix, Agenix.";
 in { "wireguard.age".publicKeys = [ key ];
-     "spotify.age".publicKeys = [ key ];}
+     "spotify.age".publicKeys = [ key ];
+     "samba-credentials.age".publicKeys = [ key ]; }
